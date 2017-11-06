c2070b869fc176f8f52fac02c9f88426d277fb43
MZeit <33051323+MZeit@users.noreply.github.com>
2017-11-06 15:44:15 +0200

    Create pluginConfig.php

---
+++ pluginConfig.php
@@ -0,0 +1,9 @@
+<?php
+// The plugin configuration settings are saved here.
+return array(
+		'google_api_key' => '******************************',
+		'SMS_service_url' => '*****************************',
+		'SMS_Provider_Username' => '*****',
+		'SMS_Provider_Passowrd' => '********'
+	);
+?>
