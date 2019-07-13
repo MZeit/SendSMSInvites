e9ef8b88ea12515ec3eb32f4cbadaa50a40071c3
MZeit <33051323+MZeit@users.noreply.github.com>
2019-07-13 11:50:08 +0300

    Update pluginConfig.php
    Commented out google API Key

--- pluginConfig.php
+++ pluginConfig.php
@@ -1,7 +1,7 @@
 <?php
 // The plugin configuration settings are saved here.
 return array(
-		'google_api_key' => '******************************',
+		//'google_api_key' => '******************************',
 		'SMS_service_url' => '*****************************',
 		'SMS_Provider_Username' => '*****',
 		'SMS_Provider_Passowrd' => '********'
